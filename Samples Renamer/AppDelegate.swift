f465347792a30e3b4231a485549d39951daadf89
chigkim <chigkim@github.com>
2018-04-12 16:37:29 -0400

    Initial Commit

---
+++ Samples Renamer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Samples Renamer
+//
+//  Created by Chi Kim on 4/12/18.
+//  Copyright © 2018 Chi Kim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Samples Renamer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Samples Renamer
+//
+//  Created by Chi Kim on 4/12/18.
+//  Copyright © 2018 Chi Kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
